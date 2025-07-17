da84ed88a7a2cb25db574e0b62b96c3e5fdd07aa
Lokesh-Pathak007 <lokeshpathak@neepanlok.com>
2025-07-17 15:10:38 +0530

    add image url

--- src/main/java/com/code/monks/nukkad/dto/response/ItemDetailsDTO.java
+++ src/main/java/com/code/monks/nukkad/dto/response/ItemDetailsDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +15,6 @@ public class ItemDetailsDTO {
   private String unit;
   private int quantity;
   private double price;
+  private List<String> imageUrls;
 }
+
