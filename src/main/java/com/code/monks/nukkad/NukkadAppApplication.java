fb0e83bfca14fc798275f3dbd55a9db8efda990d
Pankaj-Dhami007 <pankajdhami@neepanlok.com>
2025-07-30 14:16:16 +0530

    correct-newLineConflict

--- src/main/java/com/code/monks/nukkad/NukkadAppApplication.java
+++ src/main/java/com/code/monks/nukkad/NukkadAppApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class  NukkadAppApplication {
 
 
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(NukkadAppApplication.class, args);
 	}
