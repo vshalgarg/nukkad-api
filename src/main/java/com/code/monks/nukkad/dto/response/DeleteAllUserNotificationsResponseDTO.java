678dadc385cccf7499b3996c6db695fb041ee3d6
Lokesh-Pathak007 <lokeshpathak@neepanlok.com>
2025-08-05 16:39:14 +0530

    add 2 API for notification

---
+++ src/main/java/com/code/monks/nukkad/dto/response/DeleteAllUserNotificationsResponseDTO.java
@@ -0,0 +1,12 @@
+package com.code.monks.nukkad.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class DeleteAllUserNotificationsResponseDTO {
+    public String message;
+}
