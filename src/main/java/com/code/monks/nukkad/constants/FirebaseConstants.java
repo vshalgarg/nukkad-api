1ebd79504ebede30921dcfeb3028e7ceecaae034
PankajYadav <pankajyadav@codemonks.in>
2026-04-27 15:05:06 +0530

    bug fixed final

--- src/main/java/com/code/monks/nukkad/constants/FirebaseConstants.java
+++ src/main/java/com/code/monks/nukkad/constants/FirebaseConstants.java
@@ -4,6 +4,6 @@ public class FirebaseConstants {
 
     // folder name in Firebase Storage for product images
 
-    public static final String PRODUCT_IMAGE_TYPE = "products_dev";
-    public static final String CATEGORY_IMAGE_TYPE = "categories_dev";
+    public static final String PRODUCT_IMAGE_TYPE = "products";
+
 }
