ff9ab1710f86df09a0054c6b8cbe20aedbb66d0a
Pankajyadavdeveloper <pankajyadav@codemonks.in>
2026-05-18 10:18:24 +0530

    Update FirebaseConstants.java

--- src/main/java/com/code/monks/nukkad/constants/FirebaseConstants.java
+++ src/main/java/com/code/monks/nukkad/constants/FirebaseConstants.java
@@ -4,7 +4,7 @@ public class FirebaseConstants {
 
     // folder name in Firebase Storage for product images
 
-    public static final String PRODUCT_IMAGE_TYPE = "products-dev";
-    public static final String CATEGORY_IMAGE_TYPE = "category-dev";
+    public static final String PRODUCT_IMAGE_TYPE = "products_dev";
+    public static final String CATEGORY_IMAGE_TYPE = "category_dev";
 
 }
